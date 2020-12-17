03b4932d42f385c347a539f585cec2465b805c86
hemantjava <hemantjava90@gmail.com>
2020-12-17 17:35:54 +0530

    updated.

--- src/main/java/com/example/asyncspringbootexample/AsyncSpringbootExampleApplication.java
+++ src/main/java/com/example/asyncspringbootexample/AsyncSpringbootExampleApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class AsyncSpringbootExampleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/asyncspringbootexample/config/AsyncConfig.java
+++ src/main/java/com/example/asyncspringbootexample/config/AsyncConfig.java
@@ -5,9 +5,11 @@ import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig extends AsyncConfigurerSupport {
 
   @Autowired
