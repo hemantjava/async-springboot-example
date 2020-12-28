4cc92369c7b92c80254a16a949313254eb07f6f4
hemantjava <hemantjava90@gmail.com>
2020-12-28 11:53:31 +0530

    Added Custom Executor at method level.

--- src/main/java/com/example/asyncspringbootexample/service/NotificationService.java
+++ src/main/java/com/example/asyncspringbootexample/service/NotificationService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class NotificationService {
 
-  @Async
+  @Async("asyncExecutor")
   public void msgAsync() {
     log.info("thread:"+Thread.currentThread().getName());
     try {
